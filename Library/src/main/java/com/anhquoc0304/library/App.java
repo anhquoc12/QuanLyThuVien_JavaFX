f49ea117e89e5bc6f54d3fbea3829055e42af2b6
unknown <anhquoc12>
2023-03-24 14:55:51 +0700

    Event Of HomeController

--- Library/src/main/java/com/anhquoc0304/library/App.java
+++ Library/src/main/java/com/anhquoc0304/library/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Home"), 640, 480);
+        scene = new Scene(loadFXML("Home"));
         stage.setScene(scene);
         stage.show();
     }
--- Library/src/main/java/com/anhquoc0304/library/HomeController.java
+++ Library/src/main/java/com/anhquoc0304/library/HomeController.java
@@ -1,11 +1,15 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ * Click nbfs:/nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs:/nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 package com.anhquoc0304.library;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -15,12 +19,37 @@ import javafx.fxml.Initializable;
  */
 public class HomeController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    }
+
+    @FXML
+    public void BookClick(ActionEvent event) throws IOException {
+        App.setRoot("Book");
+    }
+
+    @FXML
+    public void ReaderClick(ActionEvent event) throws IOException {
+        App.setRoot("Reader");
+    }
+
+    @FXML
+    public void RentBookClick(ActionEvent event) throws IOException {
+        App.setRoot("RentBook");
+    }
+
+    @FXML
+    public void Click(ActionEvent event) throws IOException {
+        App.setRoot("Book");
+    }
+
+    @FXML
+    public void StatClick(ActionEvent event) throws IOException {
+        App.setRoot("Book");
+    }
+
+    @FXML
+    public void ExitClick(ActionEvent event) throws IOException {
+        Platform.exit();
+    }
 }
